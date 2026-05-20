2f83afc7f2c3284cf3f021131b037cd9b6ead30b
DamianCoder-100 <damianlewis494@gmail.com>
2026-05-20 08:42:38 -0400

    fix now

--- backend/back-end.js
+++ backend/back-end.js
@@ -51,8 +51,7 @@ app.use(
     origin: [
       'http://localhost:5500',
       'http://127.0.0.1:5500',
-      'http://localhost:3000',
-      'https://damian-coder-100.github.io'
+      'https://damiancoder-100.github.io'
     ]
   })
 );
